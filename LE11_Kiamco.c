3e797716b19783ae387c2394933b727a84305ee8
Kiamco-Angelo <kiamco.angelo1@gmail.com>
2025-11-06 19:38:13 +0800

    Add files via upload

---
+++ LE11_Kiamco.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    
+FILE *file = fopen("students.txt", "w");
+char name [30];
+char section [20];
+
+printf("Complete Name: ");
+fgets(name, sizeof(name), stdin);
+
+printf("Section: ");
+fgets(section, sizeof(section), stdin);
+
+file = fopen("students.txt", "w");
+if (file == NULL){
+    printf("Error opening file!\n");
+    return 1;
+}
+
+fprintf(file, "Complete Name: %s", name);
+fprintf(file, "Section: %s", section);
+fclose(file);
+
+printf("Information succesfully saved to students.txt\n");
+
+
+file = fopen("students.txt", "r");
+if (file == NULL){
+    printf("Error opening file!\n");
+    return 1;
+}
+
+char fname [30];
+char fsection [20];
+
+fgets(fname, sizeof(fname), file);
+fgets(fsection, sizeof(fsection), file);
+
+printf("\n%s", fname);
+printf("%s", fsection);
+fclose(file);
+
+return 0;
+}
